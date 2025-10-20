af3225886d440280a3f51475e81ddd5ae0dce345
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-20 21:42:26 +0530

    todo routes

---
+++ src/routes/todo.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+// import { addTodo } from "../controller/todo.controller.js";
+const route = express.Router();
+
+// route.post("/add/:userId", addTodo);
+
+
+export default route;
